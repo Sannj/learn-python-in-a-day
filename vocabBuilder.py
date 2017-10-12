75381a5a3bd86e42d8bf12624e9d610fa91139fd
sanjana <sanjanabadam@gmail.com>
2017-10-12 09:11:53 -0700

    completed vocab builder

--- vocabBuilder.py
+++ vocabBuilder.py
@@ -11,7 +11,7 @@ def list_all_words():
 
 def new_word():
 	try:
-		word, definition, hint = input('\nEnter a new word in this format(word|definition|hint)')
+		word, definition, hint = input('\nEnter a new word in this format(word|definition|hint)  ').split('|')
 		if word in vocabs:
 			print('The word already exists in the vocab list. Try again.')
 			new_word()
@@ -23,7 +23,7 @@ def new_word():
 	except:
 		print('Something is fishyy!!')
 
-def remove_a_word:
+def remove_a_word():
 	word = input('Enter a word to remove\n')
 	if word in vocabs:
 		del(vocabs[word])
@@ -33,10 +33,52 @@ def remove_a_word:
 
 
 def test_yourself():
-	score = 0;
+	score = 0
+	want_to_quit = False
 	for key, value in vocabs.items():
-		guess = input('\nGuess the word based on the definition: {}'.format(value[0]))
-		if guess == key:
-			print('\nWohoo! You guessed the word right!')
+		while True:
+			guess = input('\nGuess the word based on the definition: \n{}\n\nor[p]ass or[h]int or [q]uit\n'.format(value[0]))
+			if guess == key:
+				print('\nWohoo! You guessed the word right!')
+				score += 1
+				break
+			elif guess =='p':
+				print('The correct answer is {}.'.format(key))
+				break
+			elif guess == 'h':
+				print('The hint is: {}'.format(value[1]))
+			elif guess =='q':
+				want_to_quit = True
+			else:
+				print('\nWhoops! You got that wrong :(')
+		if want_to_quit:
+			break
+	print('You got {}/{}'.format(score, len(vocabs)))
+
+def first_method():
+	while True:
+		choice = input("""\n\nWelcome to the Vocab Builder\n\nChoose an option from the menu:\n
+			1) Test yourself
+			2) Add a new word to the vocab list
+			3) Remove a word from the list
+			4) List all the words in the vocab list
+			5) Quit\n""")
+		if choice == "1":
+			test_yourself()
+		elif choice == "2":
+			new_word()
+		elif choice == "3":
+			remove_a_word()
+		elif choice == "4":
+			list_all_words()
+		elif choice == "5":
+			break
 		else:
-			print('\nWhoops! You got that wrong :(')
+			print('Invalid option. Try again.')
+			first_method()
+
+
+
+first_method()
+
+
